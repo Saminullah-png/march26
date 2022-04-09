d3a85e6f02926e51e618c13ad7888f961f921080
Saminullah-png <samin_azrakyno1@yahoo.com>
2022-04-09 22:44:13 +0530

    css classes were changed by developers of ipl:

--- leaderBoard.js
+++ leaderBoard.js
@@ -1,10 +1,13 @@
 const request = require("request");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const fs = require("fs");
+const xlsx = require("json-as-xlsx")
 
 const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
 
 let leaderboard = [];
+let counter = 0;
 
 request(link, cb);
 
@@ -14,13 +17,17 @@ function cb(error, response, html) {
     } else {
         const dom = new JSDOM(html);
         const document = dom.window.document;
-        let allScorecardTags = document.querySelectorAll('a[data-hover="Scorecard"]');
-        for (let i = 0; i < allScorecardTags.length; i++) {
-            let link = allScorecardTags[i].href;
+        let allScorecardTags = document.querySelectorAll('.ds-border-b.ds-border-line');
+        // console.log(allScorecardTags.length);
+        for (let i = 0; i < 60; i++) {
+            let anchorTagAll = allScorecardTags[i].querySelectorAll("a");
+            let link = anchorTagAll[2].href;
             let completeLink = "https://www.espncricinfo.com" + link;
             // console.log(completeLink);
             request(completeLink, cb2);
+            counter++;
         }
+        // console.log("Line 24: ",leaderboard);
     }
 }
 
@@ -30,7 +37,7 @@ function cb2(error, response, html) {
     } else {
         const dom = new JSDOM(html);
         const document = dom.window.document;
-        let batsmenRow = document.querySelectorAll(".table.batsman tbody tr");
+        let batsmenRow = document.querySelectorAll('tbody [class="ds-border-b ds-border-line ds-text-tight-s"]');
         for (let i = 0; i < batsmenRow.length; i++) {
             let cells = batsmenRow[i].querySelectorAll("td");
             if (cells.length == 8) {
@@ -40,15 +47,37 @@ function cb2(error, response, html) {
                 let fours = cells[5].textContent;
                 let sixes = cells[6].textContent;
                 // console.log("Name : ",name,"Runs : ",runs,"Balls : ",balls,"Fours : ",fours,"Sixes : ",sixes);
+                processPlayer(name, runs, balls, fours, sixes);
             }
+        }
+        counter--;
+        if (counter == 0) {
+            console.log(leaderboard);
+            let data = JSON.stringify(leaderboard);
+            fs.writeFileSync('BatsmenStats.json', data);
+            let dataExcel = [{
+                sheet: "Ipl Stats",
+                columns: [
+                    { label: "Name", value: "Name" }, // Top level data
+                    { label: "Innings", value: "Innings" },
+                    { label: "Runs", value: "Runs" }, // Custom format
+                    { label: "Balls", value: "Balls" }, // Run functions
+                    { label: "Fours", value: "Fours" },
+                    { label: "Sixes", value: "Sixes" },
+                ],
+                content: leaderboard
+                    //[{Name:"Rahul",Innings:16,Runs:422,Balls......}]
+            }, ]
+
+            let settings = {
+                fileName: "BatsmenDetail", // Name of the resulting spreadsheet
+                extraLength: 3, // A bigger number means that columns will be wider
+                writeOptions: {}, // Style options from https://github.com/SheetJS/sheetjs#writing-options
+            }
+            xlsx(dataExcel, settings) // Will download the excel file
         }
     }
 }
-
-processPlayer('Rohit', '15', '4', '2', '4');
-processPlayer('Virat', '50', '20', '4', '3')
-processPlayer('Rohit', '40', '20', '1', '2');
-console.log(leaderboard);
 
 function processPlayer(name, runs, balls, fours, sixes) {
     runs = Number(runs);
@@ -60,6 +89,7 @@ function processPlayer(name, runs, balls, fours, sixes) {
         if (playerObj.Name == name) {
             //will do some work here
             playerObj.Runs += runs;
+            playerObj.Innings += 1;
             playerObj.Balls += balls;
             playerObj.Fours += fours;
             playerObj.Sixes += sixes;
@@ -69,6 +99,7 @@ function processPlayer(name, runs, balls, fours, sixes) {
     // code coming here means we did not find our player inside leaderboard
     let obj = {
         Name: name,
+        Innings: 1,
         Runs: runs,
         Balls: balls,
         Fours: fours,
@@ -76,3 +107,5 @@ function processPlayer(name, runs, balls, fours, sixes) {
     }
     leaderboard.push(obj);
 }
+
+// console.log("Line 80: ",leaderboard);
